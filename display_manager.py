2d8050936bd852e29c9e1d96fdd5df2974cb7136
William Legg <williamleggiv@gmail.com>
2024-10-22 16:19:21 -0400

    LCD changes

--- display_manager.py
+++ display_manager.py
@@ -1,16 +1,16 @@
-from tm1637
+from RPLCD.i2c import CharLCD
 import shutil
 
 class DisplayManager:
     def __init__(self, clk_pin, dio_pin):
-        self.display = tm1637.TM1637(clk=clk_pin, dio=dio_pin)
+        self.display = CharLCD(i2c_expander='PCF8574', address=0x27)
         self.display.brightness(1)
     
     def update_picture_count(self):
         total, used, free = shutil.disk_usage("/")
         remaining_pictures = int(free / (1.3 * 1024 * 1024))  # Assuming 1.3 MB per picture
-        self.display.number(remaining_pictures)
+        self.display_number(remaining_pictures)
     
     def display_number(self, number):
         digits = [int(d) for d in str(number).zfill(4)]
-        self.display.display(digits[:4])
+        self.display.write_string(f"Pics Left: {digits}")
